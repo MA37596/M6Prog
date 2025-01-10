488e0fcf40b57a6b8b07445515847567049bd9d1
vijf7eendrie <rayanaghmer072@outlook.com>
2025-01-10 13:42:59 +0100

    nieuwebestanden

--- m6prog_project/public/search.php
+++ m6prog_project/public/search.php
@@ -1,9 +1,39 @@
 <?php
-echo '<pre>';
-var_dump( $_GET );
-echo '</pre>';
 
-//echo '<pre>';
-//var_dump( $_GET['zoeken'] );
-//echo '</pre>';
-// dit lukte niet hij gaf de volgende error  Warning:  Undefined array key "zoeken" in /var/www/html/public/search.php on line 3
+require_once '../source/config.php';
+require_once SOURCE_ROOT . 'database.php';
+
+$sql = 'SELECT * FROM WeerPerDag WHERE plaats = ? ORDER BY Datum';
+
+$stmt = $connection->prepare($sql);
+if (!$stmt) {
+    die("Prepare failed: " . $connection->error);
+}
+
+$plaats = 'amsterdam';
+$stmt->bind_param('s', $plaats);
+
+
+if (!$stmt->execute()) {
+    die("Execution failed: " . $stmt->error);
+}
+
+$result = $stmt->get_result();
+
+
+while ($row = $result->fetch_assoc()) {
+    var_dump($row); 
+}
+
+$stmt->close();
+$connection->close();
+
+
+$data = [$plaats];
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+var_dump($data); 
+
+
