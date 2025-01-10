be7f2909800eb5b72134c1d7875e1fada6d892be
vijf7eendrie <rayanaghmer072@outlook.com>
2025-01-10 13:05:28 +0100

    nieuwebestanden

--- m6prog_project/public/nawOpslaan.php
+++ m6prog_project/public/nawOpslaan.php
@@ -1,3 +1,25 @@
 <?php
-//bij mij staat er als ik submit doe alsnog gewoon nawOpslaan.php inplaatsvan nawOpslaan.php en de informatie die ik heb ingevuld
-?>
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
+        echo '<table>';
+        echo '<tr><th>Veld</th><th>Waarde</th></tr>';
+        foreach ($_POST as $key => $value) {
+            echo '<tr><td>' . htmlspecialchars($key) . '</td><td>' . htmlspecialchars($value) . '</td></tr>';
+        }
+        echo '</table>';
+    } else {
+        echo '<p>Er zijn geen gegevens ontvangen.</p>';
+    }
+
+
+
+
+    $naam = '';
+    if ( empty( $_POST['naam']) ) {
+        echo '<b style="color:#f00;">Je moet wel je naam invullen</b>';
+    } else {
+        $naam = $_POST['naam'];
+    }
+ 
+    echo '<br>';
+    
+    ?>
