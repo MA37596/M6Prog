2d30ccbaa7ae560b0d947aa898bd4247c09a53ad
vijf7eendrie <rayanaghmer072@outlook.com>
2025-01-10 12:25:31 +0100

    nieuwebestanden

--- m6prog_project/public/search.php
+++ m6prog_project/public/search.php
@@ -1,4 +1,9 @@
 <?php
+echo '<pre>';
+var_dump( $_GET );
+echo '</pre>';
 
-
-?>
+//echo '<pre>';
+//var_dump( $_GET['zoeken'] );
+//echo '</pre>';
+// dit lukte niet hij gaf de volgende error  Warning:  Undefined array key "zoeken" in /var/www/html/public/search.php on line 3
