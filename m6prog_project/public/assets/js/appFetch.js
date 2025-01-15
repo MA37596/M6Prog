85363088267fb1aeaebc12d028b29c03838014ff
vijf7eendrie <rayanaghmer072@outlook.com>
2025-01-15 09:53:00 +0100

    nieuwebestanden

--- m6prog_project/public/assets/js/appFetch.js
+++ m6prog_project/public/assets/js/appFetch.js
@@ -1,13 +1,20 @@
 let getForm = document.getElementById("getForm");
 
-getForm.addEventListener("submit", (event) =>
-{
-
+getForm.addEventListener("submit", (event) => {
     event.preventDefault();
     toPhpWithGet(event);
+    let url = "fetchGet.php?article="+data.get("article")+"&maxprice="+data.get("maxprice"); 
+    fetch(url)
+    .then((response))=>{
+        console.log(reponse);
+    }
+
 });
 
-let form = event.target;
-const data = new FormData(form);
-console.log(data.get("article"));
-console.log(data.get("maxPrice"));
+function toPhpWithGet(event) {
+    const form = event.target; 
+    const data = new FormData(form); 
+    console.log(data.get("article"));
+    console.log(data.get("maxPrice"));
+    
+}
