08a7d1b4016be62a49092630e98880ef575b6760
vijf7eendrie <rayanaghmer072@outlook.com>
2025-01-22 23:06:12 +0100

    imagetable

---
+++ m6prog_project/sql/03 delete proberen.sql
@@ -0,0 +1,5 @@
+delete from PhotoMetadata where id =1;
+
+
+
+delete from PhotoMetadata where iso =800;
---
+++ m6prog_project/sql/03 image insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO PhotoMetadata 
+(file_name, file_path, size_in_bytes, resolution_width, resolution_height, format, camera_make, camera_model, iso, aperture, shutter_speed, focal_length, gps_latitude, gps_longitude, tags, description)
+VALUES 
+('image1.png', '/uploads/image1.png', 2048000, 1920, 1080, 'PNG', 'Canon', 'EOS 5D Mark IV', 400, 2.8, 1/125, 50.00, 52.3676, 4.9041, 'landscape, sunset', 'A beautiful sunset captured with a Canon EOS 5D Mark IV.');
+
+INSERT INTO PhotoMetadata 
+(file_name, file_path, size_in_bytes, resolution_width, resolution_height, format, camera_make, camera_model, iso, aperture, shutter_speed, focal_length, gps_latitude, gps_longitude, tags, description)
+VALUES 
+('image2.png', '/uploads/image2.png', 1500000, 1280, 720, 'PNG', 'Nikon', 'D850', 800, 3.5, 1/200, 35.00, 51.5074, 0.1278, 'portrait, nature', 'A portrait shot in nature with a Nikon D850.');
+
+INSERT INTO PhotoMetadata 
+(file_name, file_path, size_in_bytes, resolution_width, resolution_height, format, camera_make, camera_model, iso, aperture, shutter_speed, focal_length, gps_latitude, gps_longitude, tags, description)
+VALUES 
+('image3.png', '/uploads/image3.png', 1024000, 3840, 2160, 'PNG', 'Sony', 'Alpha 7R III', 1600, 4.0, 1/250, 24.00, 48.8566, 2.3522, 'cityscape, night', 'A cityscape taken at night with a Sony Alpha 7R III.');
+
+INSERT INTO PhotoMetadata 
+(file_name, file_path, size_in_bytes, resolution_width, resolution_height, format, camera_make, camera_model, iso, aperture, shutter_speed, focal_length, gps_latitude, gps_longitude, tags, description)
+VALUES 
+('image4.png', '/uploads/image4.png', 3000000, 4000, 3000, 'PNG', 'Fujifilm', 'X-T4', 200, 1.8, 1/60, 18.00, 40.7306, -73.9352, 'architecture, art', 'An architectural piece captured with a Fujifilm X-T4.');
