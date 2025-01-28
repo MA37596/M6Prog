f575baa6cbd638fc67cc16bfd2ac59b78e8fa722
vijf7eendrie <rayanaghmer072@outlook.com>
2025-01-28 11:35:22 +0100

    webcambestanden

--- m6prog_project/public/index.php
+++ m6prog_project/public/index.php
@@ -6,23 +6,5 @@ $connection = database_connect();
 
 
 
-$sql = 'SELECT * FROM Plaatsen WHERE Stad = ? ORDER BY Datum';
 
-$stmt = $connection->prepare($sql);
-if (!$stmt) {
-    die("Prepare failed: " . $connection->error);
-}
-
-$plaats = 'amsterdam';
-$stmt->bind_param('s', $plaats);
-
-if (!$stmt->execute()) {
-    die("Execution failed: " . $stmt->error);
-}
-
-$result = $stmt->get_result();
-$weerdatabase = $result->fetch_assoc();
-var_dump($weerdatabase);
-
-$stmt->close();
 $connection->close();
